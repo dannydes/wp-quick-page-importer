3ecbe20dc0d2577893b4fbfaa6a831d06243fdce
Daniel <desiradaniel2007@hotmail.com>
2015-04-01 23:32:20 +0200

    new form action URL

--- quick-page-importer.php
+++ quick-page-importer.php
@@ -35,7 +35,7 @@ class WPQPI_Settings_Page {
 		<div class="wrap">
 			<?php screen_icon(); ?>
 			<h2>Quick Page Importer</h2>
-			<form method="post" action="options.php">
+			<form method="post" action="<?php echo plugin_dir_url( __FILE__ ); ?>wpqpi-process.php">
 				<a id="file_link" class="browser button button-hero" href="#">Upload file</a>
 				<input id="file" type="file" name="file"><?php
 				submit_button();
