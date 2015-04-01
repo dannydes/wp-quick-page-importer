3efd5ec2216ee4b9dd3c88599c3201cb1ba7e5b7
Daniel <desiradaniel2007@hotmail.com>
2015-04-01 23:08:06 +0200

    main file update

--- quick-page-importer.php
+++ quick-page-importer.php
@@ -7,17 +7,48 @@
  * Author: Daniel Desira
  */
 
-require_once 'includes/options.php';
-
-function wpqpi_init_callback() {
+class WPQPI_Settings_Page {
+	
+	private $options;
+	
+	public function __construct() {
+		add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
+		add_action( 'admin_init', array( $this, 'page_init' ) );
+	}
+	
+	public function add_plugin_page() {
+		add_options_page(
+					'Settings Admin',
+					'Quick Page Importer',
+					'manage_options',
+					'wpqpi-setting-admin',
+					array( $this, 'create_admin_page' )
+				);
+	}
+	
+	public function create_admin_page() {
+		$this->options = get_option( 'wpqpi_setting' ); ?>
+		<div class="wrap">
+			<?php screen_icon(); ?>
+			<h2>Quick Page Importer</h2>
+			<form method="post" action="options.php"><?php
+				submit_button();
+			?></form>
+		</div><?php
+	}
+	
+	public function file_picker_callback() { ?>
+		<label for="file">Upload file: </label>
+		<input type="file" id="file" name="file">
+		<?php
+	}
+	
+	public function page_init() {
+		
+	}
 	
 }
 
-function wpqpi_init() {
-	add_action( 'wpqpi_init', 'wpqpi_init_callback' );
-	do_action( 'wpqpi_init' );
+if ( is_admin() ) {
+	$settings_page = new WPQPI_Settings_Page();
 }
-
-add_action( 'init', 'wpqpi_init' );
-
-?>
